35ba1d43c6a254c827923730f023e6ccac2fe466
chris-am <c.aranguizm@outlook.com>
2022-05-25 01:29:27 -0400

    declaring and using pokemon options and picture into main page

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Who's That pokemon!?</h1>
+    <PokemonPicture />
+    <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue';
+import PokemonOptions from '@/components/PokemonOptions.vue';
+export default {
+    components: { PokemonPicture, PokemonOptions }
+}
+</script>
+
+<style>
+</style>

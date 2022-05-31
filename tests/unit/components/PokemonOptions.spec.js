50dd1ac8dbb56cbcb074939f131cd612b697d2ee
chris-am <c.aranguizm@outlook.com>
2022-05-31 12:50:42 -0400

    should show all 4 options correctly

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -5,7 +5,7 @@ import { pokemon } from "../mocks/pokemon.mock"
 
 
 describe('Pokemon Options Component', () => {
-    
+
     let wrapper
 
     beforeEach(() => {
@@ -15,8 +15,28 @@ describe('Pokemon Options Component', () => {
             }
         });
     })
-
     test('Should match the snapshot', () => {
         expect(wrapper.html()).toMatchSnapshot();
     })
-} )
+    test('should show all 4 options correctly', () => {
+        const liTags = wrapper.findAll('li');
+        const [firstPkmn, secondPkmn, thirdPkmn, fourthPkmn] = wrapper.findAll('li');
+        expect(liTags.length).toBe(4)
+        expect(firstPkmn.text()).toBe('bulbasaur')
+        expect(secondPkmn.text()).toBe('ivysaur')
+        expect(thirdPkmn.text()).toBe('venusaur')
+        expect(fourthPkmn.text()).toBe('charmander')
+    })
+    test('should emmit "selection" with their respective params on click', () => {
+        const [firstPkmn, secondPkmn, thirdPkmn, fourthPkmn] = wrapper.findAll('li');
+        firstPkmn.trigger('click')
+        secondPkmn.trigger('click')
+        thirdPkmn.trigger('click')
+        fourthPkmn.trigger('click')
+        expect(wrapper.emitted('selection').length).toBe(4)
+        expect(wrapper.emitted('selection')[0]).toEqual([1])
+        expect(wrapper.emitted('selection')[1]).toEqual([2])
+        expect(wrapper.emitted('selection')[2]).toEqual([3])
+        expect(wrapper.emitted('selection')[3]).toEqual([4])
+    })
+})
